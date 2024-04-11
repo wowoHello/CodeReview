67d5edd30c01aac010efa169f60d498377e49a1d
Jay <pig22630182@gmail.com>
2024-04-11 16:46:32 +0800

    2024/04/11

--- app.js
+++ app.js
@@ -10,18 +10,28 @@ function printEvery3() {
     for (let i = 1; i <= 88; i += 3) {        
         Ans.innerHTML += "Ans: " + i + "<br />";        
     };    
-};
+}
 
 //編寫一個名為table9to9的函式，它打印出九九乘法表的內容。
 function table9to9() {
     Que.innerText = "Q.編寫一個名為table9to9的函式，它打印出九九乘法表的內容。";
     Ans.innerHTML = "";
-    
+
     for (let i = 1; i <= 9; i++) {
         for (let j = 1; j <= 9; j++){
             Ans.innerHTML += i + "x" + j + "=" +  i*j + "<br />";
         };
         Ans.innerHTML += "<br />";
     };
-};
+}
+
+//編寫一個名為isUpperCase的函式，唯一的參數是一個String，其功能為來檢查參數中String的第一個字母是否為大寫。
+function isUpperCase(str) {
+    Que.innerText = "Q.編寫一個名為isUpperCase的函式，唯一的參數是一個String，其功能為來檢查參數中String的第一個字母是否為大寫。";
+    if (str.length == 0) {
+        return false;
+    }
+    
+    return str[0] == str[0].toUpperCase();
+}
 
